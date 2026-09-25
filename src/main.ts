d7403e0b29055ddb38a79f17d3a372323dc481ec
Satria Darmawan <satria@undercurrent.tech>
2026-09-25 09:30:13 +0700

    backend starting update

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ async function bootstrap() {
   });
 
   const port = configService.get<number>('port') ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
